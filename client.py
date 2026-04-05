e43a8fd3a000d1cf8f26d4d0baad7c42f1be1122
Ian Vázquez <cam.iva@hotmail.com>
2026-04-05 18:43:21 -0300

    Envio de mensajes al servidor y el guardado en la bd modularizado

--- client.py
+++ client.py
@@ -1,30 +1,50 @@
 import socket, time
 
-HOST = 'localhost'
+HOST = "localhost"
 PORT = 5000
 
-# Configuración de IP y puerto TCP del servidor
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Validación si el servidor no se encuentra disponible
-while True:
+# Confuguración de IP y puerto TCP del servidor y validación de disponibilidad
+def validar_servidor():
     try:
-        client_socket.connect((HOST, PORT))
-        print("Conectado al servidor")
-        break
-    except ConnectionRefusedError:
-        print("Servidor no disponible. Reintentando en 3 segundos...")
-        time.sleep(3)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Configuración del envio del mensaje del cliente al servidor
-while True:
-    mensaje = input("-- ")
-    client_socket.send(mensaje.encode())
-    
-    if mensaje.lower() == 'salir':
-        break
-    
-    data = client_socket.recv(1024)
-    print(data.decode())
-    
-client_socket.close()
+        while True:
+            try:
+                client_socket.connect((HOST, PORT))
+                print("Conectado al servidor")
+                break
+            except ConnectionRefusedError:
+                print("Servidor no disponible. Reintentando en 3 segundos...")
+                time.sleep(3)
+        return client_socket
+    except Exception as e:
+        print(f"Error al validar el servidor: {e}")
+
+
+# Configuración de envio de mensajes al servidor
+def configurar_mensajes(client_socket):
+    try:
+        while True:
+            mensaje = input("-- ")
+            client_socket.send(mensaje.encode())
+
+            if mensaje.lower() == "salir":
+                break
+
+            data = client_socket.recv(1024)
+            print(data.decode())
+
+        client_socket.close()
+    except Exception as e:
+        print(f"Error al enviar el mensaje: {e}")
+
+
+def main():
+    cliente = validar_servidor()
+    if cliente:
+        configurar_mensajes(cliente)
+
+
+if __name__ == "__main__":
+    main()
